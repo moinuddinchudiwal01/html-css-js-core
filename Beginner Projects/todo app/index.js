ef39c82ab093e136f86c9e97489a559a038089c4
Moinuddin <moinuddinchudiwal@gmail.com>
2024-12-20 22:00:40 +0530

    todo functional implementation in progress

--- Beginner Projects/todo app/index.js
+++ Beginner Projects/todo app/index.js
@@ -1,1 +1,29 @@
-console.log("hello good evening");
+const generateUniqueId = () => Math.floor(1000 + Math.random() * 9000);
+const todoCollectionName = "todoCollection";
+
+const setDataToLocalStorage = (key, val) => {
+  console.log("val", val);
+  return window.localStorage.setItem(key, JSON.stringify(val));
+};
+
+const getDataTOLocalStorage = (key) => {
+  return JSON.parse(window.localStorage.getItem(key));
+};
+
+const handleCreateTodo = () => {
+  let todoInput = document.getElementById("todoInput").value;
+  if (!todoInput.trim()) return alert("Please enter todo");
+
+  let existingData = getDataTOLocalStorage(todoCollectionName);
+
+  if (existingData) {
+    setDataToLocalStorage(todoCollectionName, [
+      ...existingData,
+      { id: generateUniqueId(), value: todoInput },
+    ]);
+  } else {
+    setDataToLocalStorage(todoCollectionName, [
+      { id: generateUniqueId(), value: todoInput },
+    ]);
+  }
+};
